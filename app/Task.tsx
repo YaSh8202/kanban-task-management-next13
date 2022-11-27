56ea5cc5a08657974c334c841025b33d19493c4a
Yash <bajajy82@gmail.com>
2022-11-27 18:04:44 +0530

    added create Task Model and endpoints for tasks

--- app/Task.tsx
+++ app/Task.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { Task } from "../typings";
 
-function Task() {
+function Task({ task }: { task: Task }) {
   return (
     <div className="bg-white dark:bg-dark-side px-4 py-4 shadow-md rounded-md flex flex-col space-y-1 ">
       <h3 className="text-lg text-gray-700 dark:text-gray-100 font-bold ">
-        Buid UI for onboarding flow
+        {task.title}
       </h3>
-      <p className="text-xs text-gray-400">0 of 3 substasks</p>
+      <p className="text-xs text-gray-400">
+        {task.subtasks?.filter((s) => s.isCompleted).length} of{" "}
+        {task.subtasks?.length} substasks
+      </p>
     </div>
-  );
+  );  
 }
 
 export default Task;
